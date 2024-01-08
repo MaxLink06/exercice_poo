70df4bf6a7c3468ecd881baa8d3286fe3900abd4
Toufik1247 <127116915+Toufik1247@users.noreply.github.com>
2024-01-08 16:44:33 +0100

    Revert "add exo"

--- src/Interfaces/Recherchable.php
+++ src/Interfaces/Recherchable.php
@@ -1,9 +1,5 @@
 <?php
 
-namespace App\Interfaces;
-
-interface Recherchable
-{
+interface Recherchable {
     // Code
-    public function getPage();
 }
--- src/Models/LivreSpecialise.php
+++ src/Models/LivreSpecialise.php
@@ -1,32 +1,5 @@
 <?php
 
-namespace App\Models;
-
-use App\Interfaces\Recherchable;
-
-class LivreSpecialise extends Livre implements Recherchable
-{
-    private $Type;
-
-    public function __construct($Titre, $Author, $PublicationYear, $Type)
-    {
-        parent::__construct($Titre, $Author, $PublicationYear);
-        $this->Type = $Type;
-    }
-
-    public function getType()
-    {
-        return $this->Type;
-    }
-
-    public function setType($Type)
-    {
-        $this->Type = $Type;
-
-        return $this;
-    }
-    public function getPage()
-    {
-        return 20 * 5;
-    }
+class LivreSpecialise extends Livre {
+    // Code
 }
