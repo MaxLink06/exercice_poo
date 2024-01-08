b88b58dab2b7772230187de06d3a8a9ad4b68ba2
Dylan Olivro <dylanolivro94@gmail.com>
2024-01-08 16:42:11 +0100

    add exo

--- src/Models/Livre.php
+++ src/Models/Livre.php
@@ -1,5 +1,45 @@
 <?php
 
-class Livre extends ItemBibliotheque {
-    // Code
+namespace App\Models;
+
+class Livre
+{
+    private $Titre;
+    private $Author;
+    private $PublicationYear;
+
+    public function __construct($Titre, $Author, $PublicationYear)
+    {
+        $this->Titre = $Titre;
+        $this->Author = $Author;
+        $this->PublicationYear = $PublicationYear;
+    }
+
+    public function getTitre()
+    {
+        return $this->Titre;
+    }
+    public function setTitre($Titre)
+    {
+        return $this->Titre = $Titre;
+    }
+
+
+    public function getAuthor()
+    {
+        return $this->Author;
+    }
+    public function setAuthor($Author)
+    {
+        return $this->Author = $Author;
+    }
+
+    public function getPublicationYear()
+    {
+        return $this->PublicationYear;
+    }
+    public function setPublicationYear($PublicationYear)
+    {
+        return $this->PublicationYear = $PublicationYear;
+    }
 }
--- src/Models/LivreSpecialise.php
+++ src/Models/LivreSpecialise.php
@@ -1,5 +1,32 @@
 <?php
 
-class LivreSpecialise extends Livre {
-    // Code
+namespace App\Models;
+
+use App\Interfaces\Recherchable;
+
+class LivreSpecialise extends Livre implements Recherchable
+{
+    private $Type;
+
+    public function __construct($Titre, $Author, $PublicationYear, $Type)
+    {
+        parent::__construct($Titre, $Author, $PublicationYear);
+        $this->Type = $Type;
+    }
+
+    public function getType()
+    {
+        return $this->Type;
+    }
+
+    public function setType($Type)
+    {
+        $this->Type = $Type;
+
+        return $this;
+    }
+    public function getPage()
+    {
+        return 20 * 5;
+    }
 }
